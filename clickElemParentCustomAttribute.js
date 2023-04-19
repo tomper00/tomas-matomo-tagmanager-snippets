280520451d7e694709912836acc78fcb56fdd5dd
Tomas Persson <hypotesfabriken@gmail.com>
2023-04-19 08:55:45 +0200

    Small improvements

--- clickElemParentCustomAttribute.js
+++ clickElemParentCustomAttribute.js
@@ -1,7 +1,8 @@
 function () { 
-    if(MatomoTagManager.dataLayer.get("event").match("mtm.AllElementsClick|mtm.AllLinksClick") != null &&
-    MatomoTagManager.dataLayer.get('mtm.clickElement').parentElement.hasAttribute("aria-expanded")) {
-        return MatomoTagManager.dataLayer.get('mtm.clickElement').parentElement.getAttribute("aria-expanded");
-    }
-    else return "";
+  if (MatomoTagManager.dataLayer.get("event") === "mtm.AllElementsClick|mtm.AllLinksClick" &&
+      MatomoTagManager.dataLayer.get('mtm.clickElement').parentElement.hasAttribute("aria-expanded")) {
+    return MatomoTagManager.dataLayer.get('mtm.clickElement').parentElement.getAttribute("aria-expanded");
+  } else {
+    return null;
+  }
 }
