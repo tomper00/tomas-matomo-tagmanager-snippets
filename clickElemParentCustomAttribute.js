c243b9e7b9173634b6060e2931dfd357338ee9a6
Tomas Persson <hypotesfabriken@gmail.com>
2024-03-12 11:49:07 +0100

    Update clickElemParentCustomAttribute.js
    Fix broken if

--- clickElemParentCustomAttribute.js
+++ clickElemParentCustomAttribute.js
@@ -1,5 +1,6 @@
 function () { 
-  if (MatomoTagManager.dataLayer.get("event") === "mtm.AllElementsClick|mtm.AllLinksClick" &&
+  var regex = /mtm\.(AllElementsClick|AllLinksClick)/;
+  if (MatomoTagManager.dataLayer.get("event").match(regex) &&
       MatomoTagManager.dataLayer.get('mtm.clickElement').parentElement.hasAttribute("aria-expanded")) {
     return MatomoTagManager.dataLayer.get('mtm.clickElement').parentElement.getAttribute("aria-expanded");
   } else {
